46768f9aac7c1ac0d3349d0f03215492527dc7aa
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-16 02:42:02 -0800

    update ecs_deploy.sh remote_image_name to image_name

--- .bin/ecs_deploy.sh
+++ .bin/ecs_deploy.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 
 echo "Deploying $TRAVIS_BRANCH on $CLUSTER_NAME"
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $REMOTE_IMAGE_URL:latest
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_NAME:latest
 
 else
 echo "Skipping deploy because it's not an allowed branch"
