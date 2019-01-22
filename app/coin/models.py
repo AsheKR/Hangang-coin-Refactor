908f3e51a7f33efad9ce3f2f3442407953b6cb0a
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-22 02:24:11 -0800

    korbit이 지원하는 상위 10개의 coin을 저장하는 테스트 코드 작성

--- app/coin/models.py
+++ app/coin/models.py
@@ -1,5 +1,39 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
 from django.db import models
 
 
 class Coin(models.Model):
-    pass
+    CURRENCY_PAIR = []
+
+    name = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+
+    @classmethod
+    def get_or_create_coin_with_currency_pair(cls, currency):
+        coin, _ = Coin.objects.get_or_create(name=currency)
+        return coin
+
+    @classmethod
+    def get_top_10_coins_that_korbit_supports(cls):
+        """
+        korbit에서 지원하는 상위 10개의 코인을 가져와 Choices 형태로 저장
+
+        CURRENCY_PAIR ('CURRENCY', 'PAIR')
+        """
+        if not cls.CURRENCY_PAIR:
+            url = 'https://coinmarketcap.com/exchanges/korbit/'
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            trs = soup.select_one('table').find_all('tr')[1:]
+            for i, tr in enumerate(trs):
+                if i == 10:
+                    return
+                currency = tr.select_one('.link-secondary').text
+                pair = tr.find('a', href=re.compile(r'https://www.korbit.co.kr/*')).text
+                pair_name = re.sub(r'(\w+)/(\w+)', r'\g<1>_\g<2>', pair.lower())
+                cls.CURRENCY_PAIR.append((currency, pair_name))
--- app/coin/tests/test_models.py
+++ app/coin/tests/test_models.py
@@ -1,11 +1,15 @@
+import pytest
+
 from coin.models import Coin
 
 
 class TestCoinModel:
 
     def test_create_coin_that_korbit_supports(self):
-        currency_list = Coin.get_top_10_coins_that_korbit_supports()
+        Coin.get_top_10_coins_that_korbit_supports()
 
-        for currency, _ in currency_list:
-            coin = Coin.get_or_create_coin_with_curreny_pair(currency)
-            assert Coin.objects.get(name=currency) == coin
+        assert len(Coin.CURRENCY_PAIR) != 0, 'Crawling Currency Failed'
+
+        for currency, _ in Coin.CURRENCY_PAIR:
+            coin = Coin.get_or_create_coin_with_currency_pair(currency)
+            assert Coin.objects.get(name=currency) == coin, 'Save Currency Failed'
