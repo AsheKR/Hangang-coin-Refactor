c162d143a3e771a00725921cb7a422fc1293ce9f
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-22 01:03:57 -0800

    Korbit이 지원하는 Coin을 생성하는 테스트코드 작성

--- app/coin/tests/test_models.py
+++ app/coin/tests/test_models.py
@@ -1,4 +1,11 @@
-import pytest
+from coin.models import Coin
 
-class CoinModelTest:
-    pass
+
+class TestCoinModel:
+
+    def test_create_coin_that_korbit_supports(self):
+        currency_list = Coin.get_top_10_coins_that_korbit_supports()
+
+        for currency, _ in currency_list:
+            coin = Coin.get_or_create_coin_with_curreny_pair(currency)
+            assert Coin.objects.get(name=currency) == coin
