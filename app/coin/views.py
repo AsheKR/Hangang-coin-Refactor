c7496fd36663b1ff66fbc37c304cde5089769645
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-23 05:20:43 -0800

    home_page view에 now_value 체크 삭제

--- app/coin/views.py
+++ app/coin/views.py
@@ -13,7 +13,6 @@ def home_page(request):
 
     try:
         master_value = coin.today_master_value
-        now_value = coin.latest_value
     except AttributeError:
         return render(request, 'wait.html', )
 
