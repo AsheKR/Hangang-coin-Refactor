3bea06170c6fa437ba77956f477534b2ce8da239
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-23 21:04:31 -0800

    한강 수온이 특정상황에 나타나는지 테스트

--- app/functional_tests/base.py
+++ app/functional_tests/base.py
@@ -2,7 +2,8 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-from coin.models import Coin, CoinValue
+from coin.models import Coin
+from river.models import River
 
 
 class FunctionalTest(LiveServerTestCase):
@@ -12,6 +13,7 @@ class FunctionalTest(LiveServerTestCase):
         options.headless = True
         self.browser = webdriver.Firefox(options=options)
         self.create_coinvalue_stub()
+        self.create_river_stub()
 
     def tearDown(self):
         self.browser.quit()
@@ -19,3 +21,7 @@ class FunctionalTest(LiveServerTestCase):
     def create_coinvalue_stub(self):
         Coin.get_all_coins_with_coin_value()
         self.coin = Coin.objects.first()
+
+    def create_river_stub(self):
+        River.get_river_temperature()
+        self.river = River.objects.first()
--- app/functional_tests/test_visit_our_site.py
+++ app/functional_tests/test_visit_our_site.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 from .base import FunctionalTest
 
 
@@ -19,19 +21,22 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn(str(self.coin.today_master_value), today_master_value)
         self.assertIn(str(self.coin.latest_value), latest_value)
 
-    def test_river_temp_if_smaller_than_original_value(self):
+    def test_river_temperature_appears_in_a_particular_situation(self):
         # today_master_value보다 latest_value가 떨어졌다면 한강 수온을 가져온다.
         self.coin.coinvalue_set.create(value=self.coin.latest_value - 100)
         self.browser.get(self.live_server_url)
 
         # 한강수온이 있는지 확인한다.
         river_temperature = self.browser.find_element_by_id('river_temperature').text
-        self.assertIn(river_temperature, self.river.temperature)
+        self.assertIn(str(self.river.temperature), river_temperature)
 
         # today_master_value보다 latest_value가 올라갔다면 한강 수온을 가져오지 않는다.
         self.coin.coinvalue_set.create(value=self.coin.latest_value + 100)
         self.browser.get(self.live_server_url)
 
         # 한강수온이 있는지 확인한다.
-        river_temperature = self.browser.find_element_by_id('river_temperature').text
-        self.assertNotIn(river_temperature, self.river.temperature)
+        try:
+            river_temperature_founded = self.browser.find_element_by_id('river_temperature').size()
+            self.assertEqual(river_temperature_founded, 0)
+        except NoSuchElementException:
+            pass
