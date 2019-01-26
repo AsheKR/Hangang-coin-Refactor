187fa67f156c168f3e7b97dd115ddcb0ce077f7c
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-26 04:23:06 -0800

    ajax를 통해 coin_api를 가져옴

--- app/static/index.js
+++ app/static/index.js
@@ -3,4 +3,62 @@ $(function() {
         uri = $('input[type=radio][name=coin]:checked').val()
         window.location.href = '/coin/'+uri+'/'
     })
+    ChangeHTML.init()
 })
+
+var globalVar = (function() {
+    var _fiveSc = 0,
+        _now_coin = '';
+
+    var setNowCoin = function(now_coin) {
+        _now_coin = now_coin
+    }
+
+    var getNowCoin = function() {
+        return _now_coin
+    }
+
+    var setFiveSc = function(fiveSc) {
+        _fiveSc = fiveSc;
+    }
+
+    var getFiveSc = function() {
+        return _fiveSc;
+    }
+
+    return {
+        setFiveSc: setFiveSc,
+        getFiveSc: getFiveSc,
+        setNowCoin: setNowCoin,
+        getNowCoin: getNowCoin
+    }
+}())
+
+var ChangeHTML = {
+    init: function() {
+        globalVar.now_coin = $('input[type=radio][name=coin]:checked').val()
+        this.call_ajax()
+        this.set_interval()
+    },
+
+    call_ajax: function() {
+        url = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api/coin/' + globalVar.now_coin;
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function(data) {
+                ChangeHTML.change_html(data);
+                console.log(data);
+            }
+        })
+    },
+
+    set_interval: function() {
+        globalVar.setFiveSc(setInterval(this.call_ajax, 10000))
+    },
+
+    change_html: function() {
+
+    }
+}
