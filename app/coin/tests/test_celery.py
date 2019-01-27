dbeb506d18a338c3e3f41a40c1659fadc8e2fb89
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-26 21:39:14 -0800

    apply_async to apply

--- app/coin/tests/test_celery.py
+++ app/coin/tests/test_celery.py
@@ -4,5 +4,5 @@ from coin.tasks import get_all_coin_with_celery
 class TestCoinCeleryTask:
 
     def test_get_all_coin_method_return_status_is_success(self):
-        task = get_all_coin_with_celery.apply_async()
+        task = get_all_coin_with_celery.apply()
         assert task.status == 'SUCCESS', 'get_all_coin_method_with_celery Failed'
--- app/river/tests/test_celery.py
+++ app/river/tests/test_celery.py
@@ -4,5 +4,5 @@ from river.tasks import get_river_temperature_with_celery
 class TestRiverCeleryTask:
 
     def test_get_river_temperature(self):
-        task = get_river_temperature_with_celery.apply_async()
+        task = get_river_temperature_with_celery.apply()
         assert task.status == 'SUCCESS', 'get_all_coin_method_with_celery Failed'
