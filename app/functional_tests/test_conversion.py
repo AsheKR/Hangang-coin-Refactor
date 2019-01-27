e4552328cc485e9ffbd37b2ff3dcc16870609aa1
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-27 00:31:29 -0800

    javascript 동작이 바뀌어 기능테스트 수정

--- app/functional_tests/test_conversion.py
+++ app/functional_tests/test_conversion.py
@@ -9,6 +9,10 @@ class PageConversionTest(FunctionalTest):
         # 홈페이지를 방문한다.
         self.browser.get(self.live_server_url)
 
+        # 코인 선택창을 연다.
+        elem = self.browser.find_element_by_id('select-coin-button')
+        elem.click()
+
         # 코인을 선택한다.
         elem = self.browser.find_elements_by_css_selector("input[type='radio']")[5]
         before_checked_coin_name = elem.get_attribute('value')
