d5fe82a501b4008f2396ed51cfc9763ff47f8d26
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-28 02:20:52 -0800

    result backend 삭제

--- app/config/celery.py
+++ app/config/celery.py
@@ -4,17 +4,12 @@ from django.conf import settings
 
 if settings.DEBUG is True:
     url = 'localhost'
-    backend = 'rpc://'
 else:
     url = 'hangang-coin-redis.ke2lqx.0001.apn2.cache.amazonaws.com'
-    backend = 'redis://' + url + ':6379/0'
 
 app = Celery('tasks',
              broker='redis://'+url+':6379/0',
-             backend=backend,
              )
-
-app.conf.result_expires = 600
 
 app.conf.beat_schedule = {
     'crawling-coin-every-1-min': {
