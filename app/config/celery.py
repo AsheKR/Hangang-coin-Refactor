895275168a53523ab28b0fbe99ad58f6db3adeff
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-18 02:37:49 -0800

    Celery Sentry 연결

--- app/config/celery.py
+++ app/config/celery.py
@@ -1,7 +1,10 @@
+import logging
+
 from celery import Celery
 from celery.signals import setup_logging
 from django.conf import settings
-
+from raven import Client
+from raven.contrib.celery import register_logger_signal, register_signal
 
 if settings.DEBUG is True:
     url = 'localhost'
@@ -27,6 +30,24 @@ app.conf.update(
         },
     }
 )
+
+client = Client('https://925b7abfc0b74ef9bc6d3175516974a9:7aab90be714a49498161f704bd0df8de@sentry.io/1372674')
+
+# register a custom filter to filter out duplicate logs
+register_logger_signal(client)
+
+# The register_logger_signal function can also take an optional argument
+# `loglevel` which is the level used for the handler created.
+# Defaults to `logging.ERROR`
+register_logger_signal(client, loglevel=logging.INFO)
+
+# hook into the Celery error handler
+register_signal(client)
+
+# The register_signal function can also take an optional argument
+# `ignore_expected` which causes exception classes specified in Task.throws
+# to be ignored
+register_signal(client, ignore_expected=True)
 
 
 app.autodiscover_tasks()
